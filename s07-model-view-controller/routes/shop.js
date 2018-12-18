9fd6c6da80465cb35b27d38f9d542d0137a26e49
Jurek <jurek2006@gmail.com>
2018-12-18 14:53:31 +0100

    Start sekcji 7 - Model View Controler

---
+++ s07-model-view-controller/routes/shop.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require("express");
+const adminData = require("./admin");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+    const { products } = adminData;
+    res.render("shop", {
+        prods: products,
+        pageTitle: "Shop",
+        path: "/",
+        hasProducts: products.length > 0,
+        productCSS: true,
+        activeShop: true
+    });
+});
+
+module.exports = router;
