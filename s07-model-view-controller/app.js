0eacd1594900357b9c1cf42fecc2fd3b1fa2593a
Jurek <jurek2006@gmail.com>
2018-12-19 15:21:47 +0100

    Koniec sekcji 7

--- s07-model-view-controller/app.js
+++ s07-model-view-controller/app.js
@@ -1,25 +1,23 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
+const notFound404controller = require("./controllers/notFound404");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(notFound404controller);
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
--- s07-model-view-controller/routes/shop.js
+++ s07-model-view-controller/routes/shop.js
@@ -1,19 +1,8 @@
-const path = require("path");
 const express = require("express");
-const adminData = require("./admin");
+const productsController = require("../controllers/products");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-    const { products } = adminData;
-    res.render("shop", {
-        prods: products,
-        pageTitle: "Shop",
-        path: "/",
-        hasProducts: products.length > 0,
-        productCSS: true,
-        activeShop: true
-    });
-});
+router.get("/", productsController.getProducts);
 
 module.exports = router;
